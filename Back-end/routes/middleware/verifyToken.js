4dac393ae2936f33c4d19dcffe78ba7c09a4d75b
samkalvium <samarth.s@kalvium.community>
2024-04-08 17:04:27 +0530

    Login token done3

--- Back-end/routes/middleware/verifyToken.js
+++ Back-end/routes/middleware/verifyToken.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 const secretCode = process.env.SECRET_CODE;
 
-// MIDDLEWARE
-
+// Middleware
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1]; 
-    if (!token) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(403).json({ message: 'No token provided' });
     }
+    const token = authHeader.split(' ')[1];
     jwt.verify(token, secretCode, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
-        req.user = decoded; 
+        req.user = decoded;
         next();
     });
 };
