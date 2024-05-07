29123315842e739d2aa325d5bad236d60ac43048
samkalvium <samarth.s@kalvium.community>
2024-05-07 14:48:21 +0530

    Just Schema here

--- Back-end/models/postDB.js
+++ Back-end/models/postDB.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    "img": String,
-    "title": String,
-    "description":String
-})
+    img: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    comments: [{
+        text: [{ type: String, required: true }],
+        createdBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }]
+    }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
 
-const posts = mongoose.model('Post', postSchema)
 
-module.exports = posts;
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
