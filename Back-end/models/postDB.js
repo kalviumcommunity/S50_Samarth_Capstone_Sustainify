a85d341c542d370065c19640750cdc547cdf0da4
samkalvium <samarth.s@kalvium.community>
2024-05-07 14:56:03 +0530

    Review taken

--- Back-end/models/postDB.js
+++ Back-end/models/postDB.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
     img: { type: String, required: true },
-    title: { type: String, required: true },
+    title: { type: String, required: true, index: true }, 
     description: { type: String, required: true },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     comments: [{
         text: [{ type: String, required: true }],
         createdBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }]
     }],
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true } 
 });
-
 
 const Post = mongoose.model('Post', postSchema);
 
